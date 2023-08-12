5ea55e6d7cf6ff1baed01f90a424acb02c96dc99
isadev97 <isa325697@gmail.com>
2023-08-12 19:46:31 +0530

    new db added

--- persons_app/models.py
+++ persons_app/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+# default db_table => <app_name>_<model_name>
 class Person(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=255)
     age = models.IntegerField(default=0)
+    email = models.EmailField(null=True)
+    
+    class Meta:
+        db_table = "persons_app_person"
+    
