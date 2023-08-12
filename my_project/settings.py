005408c3bd7972bf8cae186c29dddd477dc27e6b
isadev97 <isa325697@gmail.com>
2023-08-12 18:35:46 +0530

    migration file created

--- my_project/settings.py
+++ my_project/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'persons_app'
+    'persons_app',
 ]
 
 MIDDLEWARE = [
--- persons_app/models.py
+++ persons_app/models.py
@@ -2,3 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+class Person(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    age = models.IntegerField(default=0)
