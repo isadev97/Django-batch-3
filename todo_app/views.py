b55941e86c2fe1244e0c5b8480fc2e1b445f7846
isadev97 <isa325697@gmail.com>
2023-08-13 20:21:19 +0530

    Django Template Engine

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,27 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from todo_app.models import Todo
 
 # Create your views here.
 
 def index(request):
-    html = """ 
+    todo = Todo.objects.last()
+    html = f""" 
     <h1>Hello world this an html response</h1>
     <p>Bye</p>
+    <p>Todo title : {todo.title}
     """
     return HttpResponse(html)
 
 def bye(request):
+    todo = Todo.objects.last()
     a = {
-        "title" : "hello",
-        "description" : "world"
+        "title" : todo.title
     }
     return JsonResponse(a)
 
 def hello(request):
+    todos = Todo.objects.all()
+    show = True
     page_name = "temp.html"
-    return render(request, page_name)
-
-
-
+    my_dict = {'todos' : todos, 'show': show}
+    return render(request, page_name, context=my_dict)
 
 
