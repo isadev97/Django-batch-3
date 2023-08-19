8ac0611ae181eb1bf8a17fabde19584ec002c571
isadev97 <isa325697@gmail.com>
2023-08-19 18:36:18 +0530

    todo list render page

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
-from todo_app.views import hello, index, bye # refer to views/__init__.py
+from todo_app.views import index
 from persons_app.models import Person
 
 urlpatterns = [
     path('index/', index, name='todo_index'),
-    path('bye/', bye, name='todo_bye'),
-    path('hello/', hello, name='todo_hell')
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,30 +1,16 @@
-from django.shortcuts import render
+ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from todo_app.models import Todo
 
 # Create your views here.
 
+# database <=> views <=> ui template
 def index(request):
-    todo = Todo.objects.last()
-    html = f""" 
-    <h1>Hello world this an html response</h1>
-    <p>Bye</p>
-    <p>Todo title : {todo.title}
-    """
-    return HttpResponse(html)
-
-def bye(request):
-    todo = Todo.objects.last()
-    a = {
-        "title" : todo.title
+    all_todos = Todo.objects.all()
+    data = {
+        "todos" : all_todos
     }
-    return JsonResponse(a)
-
-def hello(request):
-    todos = Todo.objects.all()
-    show = True
-    page_name = "temp.html"
-    my_dict = {'todos' : todos, 'show': show}
-    return render(request, page_name, context=my_dict)
+    page_name = "index.html"
+    return render(request, page_name, context=data)
 
 
