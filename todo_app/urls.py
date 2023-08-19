505a4b3e78d0dd80173db258bbfdfc6422b45cf4
isadev97 <isa325697@gmail.com>
2023-08-19 20:12:38 +0530

    todo app changes

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
-from todo_app.views import index
+from todo_app.views import index, add_view, delete_view
 from persons_app.models import Person
 
 urlpatterns = [
-    path('index/', index, name='todo_index'),
+    path('', index, name='todo_index'),
+    path('add-todo/', add_view, name='add_todo'),
+    path('delete-todo/<int:todo_id>', delete_view, name="todo_delete")
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,4 +1,4 @@
- from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from todo_app.models import Todo
 
@@ -6,11 +6,32 @@ from todo_app.models import Todo
 
 # database <=> views <=> ui template
 def index(request):
-    all_todos = Todo.objects.all()
+    all_todos = Todo.objects.all().order_by('title')
+    print(all_todos.query)
     data = {
         "todos" : all_todos
     }
     page_name = "index.html"
     return render(request, page_name, context=data)
+
+def add_view(request):
+    if request.method == "GET":
+        return HttpResponse("Invalid method")
+    else:
+        todo_input = request.POST['todoInput']
+        todo_object = Todo.objects.create(title=todo_input)
+        return redirect("todo_index")
+
+def delete_view(request, todo_id):
+    if request.method == "POST":
+        return HttpResponse("Invalid method")
+    else:
+        try:
+            todo_object = Todo.objects.get(id=todo_id)
+            todo_object.delete()
+            return redirect("todo_index")        
+        except Todo.DoesNotExist:
+            return HttpResponse("Error Todo not found")
+
 
 
